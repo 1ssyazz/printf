2c939f75d8d3d5f701d7d05341b4c4f76573cf87
ssyazz <sssyazwann@gmail.com>
2021-11-30 18:42:13 +0800

    edit main for deci bonus

--- main/ft_main.c
+++ main/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: msukri <msukri@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 15:35:57 by msukri            #+#    #+#             */
-/*   Updated: 2021/11/30 14:15:07 by msukri           ###   ########.fr       */
+/*   Updated: 2021/11/30 18:41:24 by msukri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ int	main(void)
 	ft_printf("String\n");
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%s|", t)));
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%1s|", "")));
-	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%.2s| |%15.9s|", t, t)));
-	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%.s|", t)));
+	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%6.9s|", t)));
+	printf("  LENGTH printf : %d\n", (printf("|%6.9s|", t)));
+	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%.2s|", t)));
 	ft_printf("Pointer\n");
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%p|", v)));
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%-1p|", 0)));
@@ -38,8 +39,11 @@ int	main(void)
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%-20.8p|", v)));
 	ft_printf("Decimal\n");
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%.0d|", 0)));
-	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%04.1d|", 32)));
+	printf("  LENGTH ft_printf : %d\n", (printf("|%.0d|", 0)));
+	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%-6.3d|", 32)));
+	printf("  LENGTH printf : %d\n", (printf("|%2.3d|", -32)));
 	ft_printf("  LENGTH ft_printf : %d\n", (ft_printf("|%04d|", 32)));
+	printf("  LENGTH printf : %d\n", (printf("|%-4.3d|", 32)));
 	ft_printf("Return:%d", (ft_printf("|%d|\n", -1)));
 	ft_printf("|%i|\n", 0);
 	ft_printf("|%i|\n", -250);
